372c607a96e8cbe81708f3bd901ab0d836b7622d
NobitaShizuka07 <97782579+NobitaShizuka07@users.noreply.github.com>
2022-03-28 23:37:04 +0530

    Update env.ts

--- src/env.ts
+++ src/env.ts
@@ -18,9 +18,9 @@ const env = cleanEnv(process.env, {
   LOG_CHANNEL: num(),
   MAX_DURATION: num({ default: 36000 }),
   THUMBNAIL: str({
-    default: 'https://telegra.ph/file/6b07279fd80ef2b844ed0.png'
+    default: 'https://te.legra.ph/file/d2f4189ab9cc2b93e1a26.jpg'
   }),
-  WATERMARK: str({ default: 'TGVCBot' })
+  WATERMARK: str({ default: 'NOBITAOPVCBot' })
 });
 
 export default env;
